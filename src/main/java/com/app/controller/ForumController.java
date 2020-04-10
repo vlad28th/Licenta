6c733a7ca2b5eeaf2ecdb425bac7f7cf38f200df
Vlad <vlad28th@yahoo.com>
2020-04-10 14:42:06 +0300

    Implement redirect for request(teacher and student)

--- src/main/java/com/app/controller/ForumController.java
+++ src/main/java/com/app/controller/ForumController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.app.model.Message;
 import com.app.model.MyUser;
+import com.app.model.Role;
 import com.app.repository.RequestRepository;
 import com.app.repository.StudentRepository;
 import com.app.repository.TeacherRepository;
@@ -38,10 +39,18 @@ public class ForumController {
 	@Autowired
 	TemeRepository projectRepo;
 	
+	@RequestMapping("/viewRequest")
+	public String redirect() {
+	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	MyUser curentUser = (MyUser)principal;
 	
+		if(curentUser.getRole().equals(Role.STUDENT))return "redirect:/studentViewRequest";
+		if(curentUser.getRole().equals(Role.TEACHER))return "redirect:/teacherViewRequest";
+    return "";
+	}
 	
-	@RequestMapping("/test")
-	public String displayStudentForum(Model model, @RequestParam("idCerere") String idCerere) {
+	@RequestMapping("/studentViewRequest")
+	public String studentViewRequest(Model model, @RequestParam("idCerere") String idCerere) {
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		MyUser curentUser = (MyUser) principal;
 		
@@ -52,7 +61,22 @@ public class ForumController {
 		
 		model.addAttribute("mesaj",mesaj);
 		
-		return "/students/forum";
+		return "/students/viewRequest";
+	}
+	
+	@RequestMapping("/teacherViewRequest")
+	public String teacherViewRequest(Model model, @RequestParam("idCerere") String idCerere) {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		MyUser curentUser = (MyUser) principal;
+		
+		Message mesaj = new Message();
+		
+		mesaj.setContent("HAI NOROC!");
+		mesaj.setRequest(requestRepo.findByIdcereri(Integer.valueOf(idCerere)));
+		
+		model.addAttribute("mesaj",mesaj);
+		
+		return "/teachers/viewRequest";
 	}
 	
 	
